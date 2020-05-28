81150fe694f0798caf28179da30337ff152529ee
Potato1682 <48394190+Potato1682@users.noreply.github.com>
2020-05-28 19:47:55 +0900

    Update check-vi.sh

--- check-vi.sh
+++ check-vi.sh
@@ -2,12 +2,9 @@ if [ -f /bin/vim ]; then
   yay -R vim --noconfirm
 fi
 
-fakeroot
-
 if [ -L /bin/vi ]; then
   sudo rm /bin/vi
 fi
-exit
 
 if [ -f /bin/vi && ! -L /bin/vi ]; then
   yay -R vi --noconfirm
