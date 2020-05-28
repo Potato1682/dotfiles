96552c10f2e74b6bf44908ccd107b96aaa1ec929
Potato1682 <48394190+Potato1682@users.noreply.github.com>
2020-05-28 17:37:16 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,29 +6,30 @@
 #
 
 DOTPATH=~/.dotfiles
-
-if has "git"; then
+if type "git" > /dev/null 2>&1; then
 	git clone --recursive "$GITHUB_URL" "$DOTPATH"
 
-elif has "curl" || has "wget"; then
+elif type "curl" > /dev/null 2>&1 || type "wget" > /dev/null 2>&1; then
 	tarball="https://github.com/Potato1682/dotfiles/archive/master.tar.gz"
 
-	if has "curl"; then
+	if type "curl" > /dev/null 2>&1; then
 		curl -L "$tarball"
 
-	elif has "wget"; then
+	elif type "wget" > /dev/null 2>&1; then
 		wget -O - "$tarball"
 	fi | tar zxv
 
 	mv -f dotfiles-master "$DOTPATH"
 
 else
-	die "curl or wget required"
+	echo "curl or wget required"
+	exit 1
 fi
 
 cd ~/.dotfiles
 if [ $? -ne 0  ]; then
-	die "not found: $DOTPATH"
+	echo "not found: $DOTPATH"
+	exit 1
 fi
 
 make install
