58db741505487c92b5543e8cf8d7312f04ea8f35
Potato1682 <ptt1682@gmail.com>
2022-08-28 23:18:11 +0900

    i'm bored

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ if [[ "$CODESPACES" ==  true ]]; then
     PERL_MM_USE_DEFAULT=1 cpan -T Stow > cpan.log 2>&1
   fi
 
-  bash -c 'source ~/.bashrc && rm -f ~/.bashrc ~/.zshrc && cd base && stow -t ~ * > stow.log 2>&1'
+  bash -c 'source ~/.bashrc && cd base && stow -t ~ * > stow.log 2>&1'
 fi
 
 exec zsh
