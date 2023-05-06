4e379dc61a4ebb9933e8bdd0fe6d39dddc672364
Potato1682 <self@potato1682.work>
2023-05-06 19:37:22 +0900

    update wayland configuration

--- base/env/.config/profile.d/20-wayland.sh
+++ base/env/.config/profile.d/20-wayland.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/sh
 
-if [ -n "$WAYLAND_DISPLAY" ]; then
-  export _JAVA_AWT_WM_NONREPARENTING=1
+export _JAVA_AWT_WM_NONREPARENTING=1
 
-  export SDL_VIDEODRIVER=wayland
-  export OBSIDIAN_USE_WAYLAND=1
-  export MOZ_ENABLE_WAYLAND=1
-  export MOZ_DBUS_REMOTE=1
-
-  export WLR_NO_HARDWARE_CURSORS=1
-fi
-
+export OBSIDIAN_USE_WAYLAND=1
+export MOZ_ENABLE_WAYLAND=1
