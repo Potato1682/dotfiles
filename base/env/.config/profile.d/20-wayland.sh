e43a0f4125653e27997b67477d7bad277cddc168
Potato1682 <self@potato1682.work>
2023-05-06 12:51:51 +0900

    add SDL wayland support

--- base/env/.config/profile.d/20-wayland.sh
+++ base/env/.config/profile.d/20-wayland.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/sh
 
-export OBSIDIAN_USE_WAYLAND=1
-export MOZ_ENABLE_WAYLAND=1
-export MOZ_DBUS_REMOTE=1
+if [ -n "$WAYLAND_DISPLAY" ]; then
+  export _JAVA_AWT_WM_NONREPARENTING=1
 
-export WLR_NO_HARDWARE_CURSORS=1
+  export SDL_VIDEODRIVER=wayland
+  export OBSIDIAN_USE_WAYLAND=1
+  export MOZ_ENABLE_WAYLAND=1
+  export MOZ_DBUS_REMOTE=1
+
+  export WLR_NO_HARDWARE_CURSORS=1
+fi
 
