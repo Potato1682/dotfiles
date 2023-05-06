1acb1aa043f74c83604018f094602f215c056e2f
Potato1682 <self@potato1682.work>
2023-05-06 12:46:02 +0900

    add PATH

--- base/env/.config/profile.d/05-path.sh
+++ base/env/.config/profile.d/05-path.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/sh
 
 # path
-export PATH="$HOME/.dotfiles/bin:$BUN_INSTALL/bin:$XDG_DATA_HOME/npm/bin:$CARGO_HOME/bin:$HOME/.local/bin:$HOME/.local/bin/tmuxes:$XDG_DATA_HOME/neovim/bin:$PATH"
+
+export PATH="$HOME/.dotfiles/bin:$GOPATH/bin:$BUN_INSTALL/bin:$XDG_DATA_HOME/npm/bin:$PNPM_HOME:$CARGO_HOME/bin:$HOME/.local/bin:$HOME/.local/bin/tmuxes:$XDG_DATA_HOME/neovim/bin:$PATH"
 
