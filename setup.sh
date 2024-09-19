0e8bbfcc9108cf1b6cd7d982a809e7169a38ed23
Potato1682 <self@potato1682.work>
2024-09-19 19:25:29 +0900

    correct installation

--- setup.sh
+++ setup.sh
@@ -5,6 +5,18 @@ if [[ "$CODESPACES" ==  true ]]; then
     PERL_MM_USE_DEFAULT=1 cpan -T Stow > cpan.log 2>&1
   fi
 
+  if [ -z "$XDG_DATA_HOME" ]; then
+    mkdir -p "$HOME"/.local/share
+  fi
+
+  if [ -z "$XDG_STATE_HOME" ]; then
+    mkdir -p "$HOME"/.local/state
+  fi
+
+  if [ -z "$XDG_CONFIG_HOME" ]; then
+    mkdir -p "$HOME"/.config
+  fi
+
   bash -c 'source ~/.bashrc && cd base && stow -t ~ * > stow.log 2>&1'
 fi
 
