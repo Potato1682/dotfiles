df2d3668157cc1c8ad5919635e05915732ed2c01
Potato1682 <self@potato1682.work>
2024-09-19 19:13:28 +0900

    fix env loading

--- base/env/.config/profile.d/00-sd-env.sh
+++ base/env/.config/profile.d/00-sd-env.sh
@@ -1,4 +1,12 @@
-export $(run-parts /usr/lib/systemd/user-environment-generators | sed '/:$/d; /^$/d' | xargs -d'\n') ENV_LOADED=1
+if [ -d /usr/lib/systemd/user-environment-generators ]; then
+  export $(run-parts /usr/lib/systemd/user-environment-generators | sed '/:$/d; /^$/d' | xargs -d'\n') ENV_LOADED=1
+else
+  _ENV_LOAD_EVAL="export $(cat "${XDG_CONFIG_HOME:-$HOME/.config}"/environment.d/*.conf | sed '/:$/d; /^$/d' | xargs -d'\n') ENV_LOADED=1"
 
-dbus-update-activation-environment --systemd --all
+  eval "$_ENV_LOAD_EVAL"
+fi
+
+if command -v dbus-update-activation-environment >/dev/null; then
+  dbus-update-activation-environment --systemd --all
+fi
 
